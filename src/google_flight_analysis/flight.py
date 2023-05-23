ed4b0e68bea6feed469b1bc6ea785a98340f32cf
Emanuele Salonico <esalonico@gmail.com>
2023-05-23 13:39:08 +0200

    initial commit

---
+++ src/google_flight_analysis/flight.py
@@ -0,0 +1,287 @@
+# Inspired and adapted from https://pypi.org/project/google-flight-analysis/
+# author: Emanuele Salonico, 2023
+
+from datetime import date, datetime, timedelta
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import re
+
+__all__ = ['Flight']
+
+
+class Flight:
+
+    def __init__(self, dl, roundtrip, price_trend, *args):
+        self._roundtrip = roundtrip
+        self._id = 1
+        self._origin = None
+        self._dest = None
+        self._date = dl
+        self._dow = datetime.strptime(dl, '%Y-%m-%d').isoweekday() # day of week
+        self._airline = None
+        self._flight_time = None
+        self._num_stops = None
+        self._stops = None
+        self._stops_locations = None
+        self._co2 = None
+        self._emissions = None
+        self._price = None
+        self._price_trend = price_trend
+        self._times = []
+        self._time_leave = None
+        self._time_arrive = None
+        self._trash = []
+        self._parse_args(*args)
+
+    def __repr__(self):
+        return f"{self._id}-{self._origin}-{self._dest}-{self._date}"
+
+    # def __str__(self):
+    #     return "__str__ To be Implemented"
+
+    @property
+    def roundtrip(self):
+        return self._roundtrip
+    
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def origin(self):
+        return self._origin
+
+    @origin.setter
+    def origin(self, x : str) -> None:
+        self._origin = x
+
+    @property
+    def dest(self):
+        return self._dest
+
+    @dest.setter
+    def dest(self, x : str) -> None:
+        self._dest = x
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, x : str) -> None:
+        self._date = x
+
+    @property
+    def dow(self):
+        return self._dow
+
+    @property
+    def airline(self):
+        return self._airline
+
+    @property
+    def flight_time(self):
+        return self._flight_time
+
+    @property
+    def num_stops(self):
+        return self._num_stops
+
+    @property
+    def stops(self):
+        return self._stops
+
+    @property
+    def stops_locations(self):
+        return self._stops_locations
+
+    @property
+    def co2(self):
+        return self._co2
+
+    @property
+    def emissions(self):
+        return self._emissions
+
+    @property
+    def price(self):
+        return self._price
+    
+    @property
+    def price_trend(self):
+        return self._price_trend
+
+    @property
+    def time_leave(self):
+        return self._time_leave
+
+    @property
+    def time_arrive(self):
+        return self._time_arrive
+
+
+    def _classify_arg(self, arg):
+        if arg == "Change of airport":
+            self._stops = self._stops_locations = "Change of airport"
+            return
+        elif arg in ["round trip", "Climate friendly"]:
+            return
+
+        # arrival or departure time
+        if ('AM' in arg or 'PM' in arg) and len(self._times) < 2:
+            delta = timedelta(days = 0)
+            if arg[-2] == '+':
+                delta = timedelta(days = int(arg[-1]))
+                arg = arg[:-2]
+
+            date_format = "%Y-%m-%d %I:%M%p"
+            self._times += [datetime.strptime(self._date + " " + arg, date_format) + delta]
+
+        # flight time        
+        elif ('hr' in arg or 'min'in arg) and self._flight_time is None:
+            self._flight_time = arg
+   
+        # number of stops
+        elif 'stop' in arg and self._num_stops is None:
+            self._num_stops = 0 if arg == 'Nonstop' else int(arg.split()[0])
+
+        # co2 
+        elif arg.endswith('CO2') and self._co2 is None:
+            self._co2 = int(arg.split()[0])
+        
+        # emissions
+        elif arg.endswith('emissions') and self._emissions is None:
+            emission_val = arg.split()[0]
+            self._emissions = 0 if emission_val == 'Avg' else int(emission_val[:-1])
+        
+        # price
+        elif arg.replace(',','').isdigit() and self._price is None:
+            self._price = int(arg.replace(',',''))
+   
+        # origin/dest        
+        elif (len(arg) == 6 and arg.isupper() or "Flight + Train" in arg) and (self._origin is None) and (self._dest is None):
+            if "Flight + Train" in arg:
+                self._origin = self._dest = "Flight + Train"
+            else:
+                self._origin = arg[:3]
+                self._dest = arg[3:]
+
+        # layover (1 stop + time at stop or multiple stops)
+        elif (('hr' in arg or "min" in arg)) or (len(arg.split(', ')) > 1 and arg.isupper()):
+            # stops locations: 1 location
+            if (arg[-3:].isupper() or arg.split(" ")[-1][0].isupper()) and ("," not in arg):
+                self._stops = arg[:-3]
+                if arg[-3:].isupper():
+                    self._stops_locations = arg[-3:]
+                else:
+                    self._stops_locations = arg.split(" ")[-1]
+            # 2 or more
+            else:
+                self._stops = arg
+                self._stops_locations = arg
+        
+        # airline
+        elif len(arg) > 0:
+            if "Operated" in arg:
+                airline = arg.split("Operated")[0]
+            else:
+                airline = arg
+    
+            self._airline = airline
+        
+        # other
+        else:
+            self._trash += [arg]
+            # airline and other stuff idk
+
+        if len(self._times) == 2:
+            self._time_leave = self._times[0]
+            self._time_arrive = self._times[1]
+
+    def _parse_args(self, args):
+        for arg in args:
+            self._classify_arg(arg)
+
+    @staticmethod
+    def get_duration_from_string(s):
+        if not bool(re.search("hr|min", str(s))):
+            return s
+        
+        h = 0
+        m = 0
+        
+        if "hr" in s:
+            h = int(s.split("hr")[0])
+        if "min" in s:
+            m = int(re.split("hr|min", s)[-2])
+
+        # return timedelta(hours=h, minutes=m)
+        return f"{h:02d}:{m:02d}"
+        
+    
+    @staticmethod
+    def dataframe(flights):
+        data = {
+            'Departure datetime': [],
+            'Arrival datetime': [],
+            'Airline(s)' : [],
+            'Travel Time' : [],
+            'Origin' : [],
+            'Destination' : [],
+            'Num Stops' : [],
+            'Layover' : [],
+            'Stops Location' : [],
+            # 'CO2 Emission (kg)' : [],
+            # 'Emission Diff (%)' : [],
+            'Price (€)' : [],
+            'Price Trend' : [],
+            'Price Value' : [],
+            'Access Date' : [],
+            'Flight Type' : []
+            
+        }
+
+        for flight in flights:
+            data['Departure datetime'] += [flight.time_leave]
+            data['Arrival datetime'] += [flight.time_arrive]
+            data['Airline(s)'] += [flight.airline]
+            data['Travel Time'] += [flight.flight_time]
+            data['Origin'] += [flight.origin]
+            data['Destination'] += [flight.dest]
+            data['Num Stops'] += [flight.num_stops]
+            data['Layover'] += [flight.stops]
+            data['Stops Location'] += [flight.stops_locations]
+            # data['CO2 Emission (kg)'] += [flight.co2]
+            # data['Emission Diff (%)'] += [flight.emissions]
+            data['Price (€)'] += [flight.price]
+            data["Price Trend"] += [flight.price_trend[0]]
+            data["Price Value"] += [flight.price_trend[1]]
+            data['Access Date'] += [datetime.today().replace(hour = 0, minute = 0, second = 0, microsecond = 0)]
+            data['Flight Type'] += [("Roundtrip" if flight.roundtrip else "One Way")]
+            
+        df = pd.DataFrame(data)
+        
+        # further cleaning
+		# convert: travel time to duration
+        df['Travel Time'] = df['Travel Time'].apply(lambda x: Flight.get_duration_from_string(x))
+        df['Layover'] = df['Layover'].apply(lambda x: Flight.get_duration_from_string(x))
+        
+        # add column: Days in Advance
+        df['Days in Advance'] = (df['Departure datetime'] - df['Access Date']).dt.days
+
+        return df
+     
+    @staticmethod
+    def assert_error(x, arg): # TODO: this is outdated
+        return [
+            "Parsing Arg 0 as Date Leave elem is incorrect.",
+            "Parsing Arg 1 as Date Return elem is incorrect.",
+            -1,
+            -1,
+            -1,
+            "Parsing Arg 6 as num stop elem is incorrect."
+            "Parsing Arg 7 as CO2 elem is incorrect.",
+            "Parsing Arg 8 as emissions elem is incorrect.",
+            "Parsing Arg 9 as price elem is incorrect."
+        ][x] + ": " + arg 
---
+++ src/google_flight_analysis/scrape.py
@@ -0,0 +1,283 @@
+# Inspired and adapted from https://pypi.org/project/google-flight-analysis/
+# author: Emanuele Salonico, 2023
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import date, datetime, timedelta
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+import sys
+sys.path.append('src/google_flight_analysis')
+from flight import *
+
+__all__ = ['Scrape', '_Scrape']
+
+class _Scrape:
+
+    def __init__(self):
+        self._origin = None
+        self._dest = None
+        self._date_leave = None
+        self._date_return = None
+        self._round_trip = None
+        self._data = None
+
+        self.results_dirty = None
+        self.results_clean = None
+        self.url = None
+
+
+    def __call__(self, *args):
+        if len(args) <= 4:
+            # base call protocol
+            self._set_properties(*args)
+            self._data = self._scrape_data()
+            obj = self.clone(*args)
+            
+            obj.data = self._data
+            obj.results_clean = self.results_clean
+            obj.results_dirty = self.results_dirty
+            obj.url = self.url
+            
+            return obj
+        else:
+            # data file being added to new scrape
+            self._set_properties(*(args[:-1]))
+            obj = self.clone(*(args[:-1]))
+            obj.data = args[-1]
+            return obj
+
+    def __str__(self):
+        if self._date_return is None:
+            return "{dl}: {org} --> {dest}".format(
+            dl = self._date_leave,
+            org = self._origin,
+            dest = self._dest
+        )
+        else:
+            return "{dl}: {org} --> {dest}\n{dr}: {dest} --> {org}".format(
+                dl = self._date_leave,
+                dr = self._date_return,
+                org = self._origin,
+                dest = self._dest
+            )
+
+    def __repr__(self):
+        if self._date_return is None:
+            return "{n} RESULTS FOR:\n{dl}: {org} --> {dest}".format(
+                n = self._data.shape[0],
+                dl = self._date_leave,
+                org = self._origin,
+                dest = self._dest
+            )
+        else:
+            return "{n} RESULTS FOR:\n{dl}: {org} --> {dest}\n{dr}: {dest} --> {org}".format(
+                n = self._data.shape[0],
+                dl = self._date_leave,
+                dr = self._date_return,
+                org = self._origin,
+                dest = self._dest
+            )
+
+    def clone(self, *args):
+        obj = _Scrape()
+        obj._set_properties(*args)
+        return obj
+
+    '''
+        Set properties upon scraper called.
+    '''
+    def _set_properties(self, *args):
+        if len(args) >= 4:
+            prop = args
+        else:
+            prop = (args + (None,))
+
+        (self._origin, self._dest, self._date_leave, self._date_return) = prop
+        
+        # compute if the trip is a round trip
+        self._round_trip = (False if self._date_return is None else True)
+  
+        
+
+    @property
+    def origin(self):
+        return self._origin
+
+    @origin.setter
+    def origin(self, x : str) -> None:
+        self._origin = x
+
+    @property
+    def dest(self):
+        return self._dest
+
+    @dest.setter
+    def dest(self, x : str) -> None:
+        self._dest = x
+
+    @property
+    def date_leave(self):
+        return self._date_leave
+
+    @date_leave.setter
+    def date_leave(self, x : str) -> None:
+        self._date_leave = x
+
+    @property
+    def date_return(self):
+        return self._date_return
+
+    @date_return.setter
+    def date_return(self, x : str) -> None:
+        self._date_return = x
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, x):
+        self._data = x
+
+    '''
+        Scrape the object
+    '''
+    def _scrape_data(self):
+        driver = webdriver.Chrome()
+        # driver.maximize_window()
+        url = self._make_url()
+        self.url = url
+        flight_results = self._get_results(url, driver)
+        driver.quit()
+        return flight_results
+
+
+    def _make_url(self):
+        if self._round_trip:
+            return 'https://www.google.com/travel/flights?q=Flights%20to%20{dest}%20from%20{org}%20from%20{date_leave}%20to%20{date_return}'.format(
+                dest = self._dest,
+                org = self._origin,
+                date_leave = self._date_leave,
+                date_return = self._date_return)
+        else:
+            return 'https://www.google.com/travel/flights?q=Flights%20to%20{dest}%20from%20{org}%20on%20{date_leave}%20oneway'.format(
+                dest = self._dest,
+                org = self._origin,
+                date_leave = self._date_leave
+            )
+
+
+    def _get_results(self, url, driver):
+        results = None
+        try:
+            results = _Scrape._make_url_request(url, driver)
+            self.results_dirty = results
+        except TimeoutException:
+            print(
+                '''TimeoutException, try again and check your internet connection!\n
+                Also possible that no flights exist for your query :('''.replace('\t','')
+            )
+            return -1
+
+        flights = self._clean_results(results)
+        self.results_clean = flights
+        return Flight.dataframe(flights)
+
+    def _clean_results(self, result):
+        res2 = [x.encode("ascii", "ignore").decode().strip() for x in result]
+        
+        price_trend_dirty = [x for x in res2 if x.startswith("Prices are currently")][0]
+        price_trend = Scrape.extract_price_trend(price_trend_dirty)
+
+        start = res2.index("Sort by:")+1
+        mid_start = res2.index("Price insights")
+        mid_end = -1
+        try:
+            mid_end = res2.index("Other departing flights")+1
+        except:
+            mid_end = res2.index("Other flights")+1
+        end  = [i for i, x in enumerate(res2) if x.endswith('more flights')][0]
+
+        res3 = res2[start:mid_start] + res2[mid_end:end]
+
+        matches = [i for i, x in enumerate(res3) if len(x) > 2 and ((x[-2] != '+' and (x.endswith('PM') or x.endswith('AM'))) or x[-2] == '+')][::2]
+
+
+        flights = [
+            Flight(
+                self._date_leave,
+                self._round_trip,
+                price_trend,
+                res3[matches[i]:matches[i+1]]) for i in range(len(matches)-1)
+            ]
+
+        return flights
+
+
+    @staticmethod
+    def extract_price_trend(s):
+        if s == "Prices are currently typical":
+            return ("typical", None)
+        
+        elif s == "Prices are currently high":
+            return ("high", None)
+        
+        elif "cheaper" in s:
+            how_cheap = int([x for x in s.split(" ") if x.isdigit()][0])
+            return ("low", how_cheap)
+        
+        else:
+            return (None, None)
+        
+        
+    @staticmethod
+    def _get_driver():
+        driver = None
+        try:
+            driver = webdriver.Chrome() # driver file path could be added here as an argument
+        except:
+            raise Exception(
+                '''Appropriate ChromeDriver version not found.\n
+                Make sure Chromedriver is downloaded with appropriate version of Chrome.\n
+                In Chrome, Go to Settings --> About Chrome to find version.\n
+                Visit https://chromedriver.chromium.org and download matching ChromeDriver version.
+                '''
+            )
+   
+    @staticmethod
+    def _identify_google_terms_page(page_source: str):
+        if "Before you continue to Google" in page_source:
+            return True
+        return False
+    
+    @staticmethod
+    def _make_url_request(url, driver):
+        """
+        Also handles the Google's Terms & Conditions page.
+        """
+        timeout = 15
+        driver.get(url)
+
+        # detect Google's Terms & Conditions page
+        WebDriverWait(driver, timeout).until(lambda s: Scrape._identify_google_terms_page(s.page_source))
+        
+        # click on accept terms button
+        WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Accept all')]"))).click()
+
+        # wait for flight data to load and initial XPATH cleaning
+        WebDriverWait(driver, timeout).until(lambda d: len(_Scrape._get_flight_elements(d)) > 100)
+        results = _Scrape._get_flight_elements(driver)
+
+        return results
+
+    @staticmethod
+    def _get_flight_elements(driver):
+        return driver.find_element(by = By.XPATH, value = '//body[@id = "yDmH0d"]').text.split('\n')
+
+Scrape = _Scrape()
